c2154ffb10f3f1c1570ce2d2b59185f1a90ba9b9
pguma <p.guma.service+git@gmail.com>
2024-03-07 18:04:48 +0900

    add easy http server

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	// Support for structured logs
 	"log/slog"
 	"os"
@@ -14,4 +16,16 @@ func init() {
 func main() {
 	fmt.Println("Hello Golang Labo")
 	slog.Info("Hello Golang Labo", slog.String("event", "sample logging"))
+
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	)
+
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
 }
