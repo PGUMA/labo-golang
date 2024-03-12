a865b7507ac3e2b739ab5849afd3f35d919f77f7
pguma <p.guma.service+git@gmail.com>
2024-03-12 17:43:06 +0900

    mod can set port num

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 
 	// Support for structured logs
@@ -16,9 +18,8 @@ func init() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -27,7 +28,7 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
 			slog.Error("failed to close: %+v", err)
 			return err
 		}
@@ -43,5 +44,19 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	// no process
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server* %v", err)
+		os.Exit(1)
+	}
 }
