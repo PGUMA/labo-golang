c29fd79d41a65874c32f9b2623d85538b4bb1226
pguma <p.guma.service+git@gmail.com>
2024-03-12 17:26:37 +0900

    mod change main testable run function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PGUMA/labo-golang
 
 go 1.21.6
+
+require golang.org/x/sync v0.6.0
--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	// Support for structured logs
 	"log/slog"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func init() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 }
 
-func main() {
-	fmt.Println("Hello Golang Labo")
-	slog.Info("Hello Golang Labo", slog.String("event", "sample logging"))
-
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		slog.Error("failed to shoutdown: %+v", err)
+	}
+
+	return eg.Wait()
+}
+
+func main() {
+	// no process
 }
